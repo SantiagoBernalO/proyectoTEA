8ad85ca3067afe0835d1615ba44c7e8d06e5c5c5
SantiagoBernalo <asantibo129@gmail.com>
2023-11-28 18:12:18 -0500

    ajuste visibilidad del cursor unicamente en computadoras

--- src/app/follow-cursor/follow-cursor.component.ts
+++ src/app/follow-cursor/follow-cursor.component.ts
@@ -1,19 +1,25 @@
+import { Platform } from '@angular/cdk/platform';
 import { Component, HostListener, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-follow-cursor',
-  template: `<img src="assets/image/cursor.png" [style.left.px]="left" [style.top.px]="top" />`,
+  template: `<img *ngIf="showCursor" src="assets/image/cursor.png" [style.left.px]="left" [style.top.px]="top" />`,
   styleUrls: ['./follow-cursor.component.css']
 })
 export class FollowCursorComponent {
   left = 0;
   top = 0;
+  showCursor: Boolean = true;
 
-  constructor(private el: ElementRef, private renderer: Renderer2) { }
+  constructor(private el: ElementRef, private renderer: Renderer2, private platform: Platform) {
+    this.showCursor = !(this.platform.IOS || this.platform.ANDROID);
+  }
 
   @HostListener('document:mousemove', ['$event'])
   onMouseMove(event: MouseEvent) {
-    this.left = (event.pageX)-22;
-    this.top = event.pageY;
+    if (this.showCursor) {
+      this.left = (event.pageX)-22;
+      this.top = event.pageY;
+    }
   }
 }
